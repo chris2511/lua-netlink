cf5e28a6d567045a6723daf09777ccb6da4ca994
Christian Hohnstaedt <christian@hohnstaedt.de>
2021-12-01 07:23:47 +0100

    chaeck.sh: remove --std=posix option
    cppcheck: Option --std=posix is deprecated and will be removed in 2.05

--- check.sh
+++ check.sh
@@ -2,7 +2,6 @@
 
 luacheck examples || r=1
 
-cppcheck --enable=all --std=posix\
-	 --suppressions-list=CppCheckSuppressions.txt src/*.c || r=1
+cppcheck --enable=all --suppressions-list=CppCheckSuppressions.txt src/*.c || r=1
 
 exit $r
