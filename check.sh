4e8df98df5e7b4090cc4d12fb5a0165916b2f2a3
Christian Hohnstaedt <christian@hohnstaedt.de>
2022-05-03 21:28:13 +0200

    Add github workflows

--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
+rm -rf build && cmake -B build && cmake --build build || r=1
 luacheck examples || r=1
-
 cppcheck --enable=all --suppressions-list=CppCheckSuppressions.txt src/*.c || r=1
 
 exit $r
